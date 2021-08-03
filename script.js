10c0a1ba118ff88672b96c7ee54f3c122532f80e
Haley Morris <haleymor@smartt.email>
2021-08-02 21:25:16 -0700

    responsive design

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
-let sliderImages = document.querySelectorAll('.slide');
-let arrowLeft = document.querySelector('#arrow-left');
-let arrowRight = document.querySelector('#arrow-right');
-let current = 0;
 
-// clear all images
-function reset() {
-  for(let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = 'none';
-  }
-};
 
-//start slide show
-function startSlide() {
-  reset();
-  sliderImages[0].style.display = 'block';
-};
+// mobile menu variables
+const mobileMenu = {};
+const mainMenu = document.querySelector('.main-menu');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.open-menu');
+const logo = document.querySelector('.logo');
+const bookNow = document.querySelector('.slider-button');
+const card = document.querySelectorAll('.card-inner');
 
-// show previous - slide left
-function slideLeft() {
-  reset();
-  sliderImages[current - 1].style.display = 'block';
-  current--;
-}
 
-// show next - slide right
-function slideRight() {
-  reset();
-  sliderImages[current + 1].style.display = 'block';
-  current++;
-}
-
-//left arrow click
-arrowLeft.addEventListener('click', function() {
-  if(current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
+card.forEach((card) => {
+  card.addEventListener('click',() => {
+    card.classList.toggle('is-flipped');
+  });
 });
 
-//right arrow click
-arrowRight.addEventListener('click', function() {
-  if(current === sliderImages.length -1) {
-    current = -1;
+mobileMenu.init = function () {
+
+  openMenu.addEventListener('click', show);
+  closeMenu.addEventListener('click', close);
+
+
+  function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    bookNow.style.visibility = 'hidden';
   }
-  slideRight();
+
+  function close() {
+    mainMenu.style.top = '-100%';
+    // logo.style.display = 'none';
+    bookNow.style.visibility = 'visible';
+  }
+
+};
+
+
+$(function () {
+  mobileMenu.init();
 });
-
-startSlide();
-
-
-
-
-
