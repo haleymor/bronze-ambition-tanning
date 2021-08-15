71c80c7ab122af4c6c0c7f9c0d850bce7550c955
Haley Morris <haleymor@smartt.email>
2021-08-14 20:21:29 -0700

    latest feedback

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const logo = document.querySelector('.logo');
 const bookNow = document.querySelector('.slider-button');
 const card = document.querySelectorAll('.card-inner');
 
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+
 
 card.forEach((card) => {
   card.addEventListener('click',() => {
@@ -40,3 +45,48 @@ mobileMenu.init = function () {
 $(function () {
   mobileMenu.init();
 });
+
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+// Init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function () {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
