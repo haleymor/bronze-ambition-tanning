79102f9f3a791343bc94d9e94632577d6693ba88
Haley Morris <haleymor@smartt.email>
2021-09-12 17:11:58 -0400

    added auto rotation for slider

--- script.js
+++ script.js
@@ -46,50 +46,123 @@ $(function () {
   mobileMenu.init();
 });
 
-// Clear all images
-function reset() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = "none";
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load", function () {
+  showSlides(slideIndex);
+  myTimer = setInterval(function () { plusSlides(1) }, 4000);
+
+  //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  slideshowContainer = document.getElementsByClassName('slider')[0];
+
+  //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
+  // slideshowContainer.addEventListener('mouseenter', pause)
+  // slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n) {
+  clearInterval(myTimer);
+  if (n < 0) {
+    showSlides(slideIndex -= 1);
+  } else {
+    showSlides(slideIndex += 1);
+  }
+
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+  if (n === -1) {
+    myTimer = setInterval(function () { plusSlides(n + 2) }, 4000);
+  } else {
+    myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
   }
 }
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  // var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+}
+
+// pause = () => {
+//   clearInterval(myTimer);
+// }
+
+// resume = () => {
+//   clearInterval(myTimer);
+//   myTimer = setInterval(function () { plusSlides(slideIndex) }, 4000);
+// }
+
+
+// Clear all images
+// function reset() {
+//   for (let i = 0; i < sliderImages.length; i++) {
+//     sliderImages[i].style.display = "none";
+//   }
+// }
 
 // Init slider
-function startSlide() {
-  reset();
-  sliderImages[0].style.display = "block";
-}
+// function startSlide() {
+//   reset();
+//   sliderImages[0].style.display = "block";
+// }
 
-// Show prev
-function slideLeft() {
-  reset();
-  sliderImages[current - 1].style.display = "block";
-  current--;
-}
+// // Show prev
+// function slideLeft() {
+//   reset();
+//   sliderImages[current - 1].style.display = "block";
+//   current--;
+// }
 
-// Show next
-function slideRight() {
-  reset();
-  sliderImages[current + 1].style.display = "block";
-  current++;
-}
+// // Show next
+// function slideRight() {
+//   reset();
+//   sliderImages[current + 1].style.display = "block";
+//   current++;
+// }
 
-// Left arrow click
-arrowLeft.addEventListener("click", function () {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
-});
 
-// Right arrow click
-arrowRight.addEventListener("click", function () {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-});
+// // Left arrow click
+// arrowLeft.addEventListener("click", function () {
+//   if (current === 0) {
+//     current = sliderImages.length;
+//   } 
+//   slideLeft();
+// });
 
-startSlide();
+// // Right arrow click
+// arrowRight.addEventListener("click", function () {
+//   if (current === sliderImages.length - 1) {
+//     current = -1;
+//   }
+//   slideRight();
+// });
+
+
+// startSlide();
 
 function enableBtn() {
   document.getElementById("submitBtn").disabled = false;
