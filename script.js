f6c2df74b68dc94becb921cfd3e80ccaffc9552e
Haley Morris <haleymor@smartt.email>
2021-11-20 19:01:40 -0500

    latest slider and small updates

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ const logo = document.querySelector('.logo');
 const bookNow = document.querySelector('.slider-button');
 const card = document.querySelectorAll('.card-inner');
 
-let sliderImages = document.querySelectorAll(".slide"),
-  arrowLeft = document.querySelector("#arrow-left"),
-  arrowRight = document.querySelector("#arrow-right"),
-  current = 0;
+// let sliderImages = document.querySelectorAll(".slide"),
+//   arrowLeft = document.querySelector("#arrow-left"),
+//   arrowRight = document.querySelector("#arrow-right"),
+//   current = 0;
+
 
 
 card.forEach((card) => {
@@ -46,27 +47,9 @@ $(function () {
   mobileMenu.init();
 });
 
-var slideIndex = 1;
 
-var myTimer;
+//SLIDER JS
 
-var slideshowContainer;
-
-window.addEventListener("load", function () {
-  showSlides(slideIndex);
-  myTimer = setInterval(function () { plusSlides(1) }, 4000);
-
-  //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-  slideshowContainer = document.getElementsByClassName('slider')[0];
-
-  //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-  // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
-
-  // slideshowContainer.addEventListener('mouseenter', pause)
-  // slideshowContainer.addEventListener('mouseleave', resume)
-})
-
-// NEXT AND PREVIOUS CONTROL
 function plusSlides(n) {
   clearInterval(myTimer);
   if (n < 0) {
@@ -74,9 +57,6 @@ function plusSlides(n) {
   } else {
     showSlides(slideIndex += 1);
   }
-
-  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-
   if (n === -1) {
     myTimer = setInterval(function () { plusSlides(n + 2) }, 4000);
   } else {
@@ -84,28 +64,120 @@ function plusSlides(n) {
   }
 }
 
-//Controls the current slide and resets interval if needed
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// Thumbnail image controls
 function currentSlide(n) {
   clearInterval(myTimer);
-  myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+  myTimer = setInterval(function () { plusSlides(n + 1) }, 5000);
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides (n) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  // var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", " ");
+  }
+  slides[slideIndex-1].style.display = "block";
+
+  // dots[slideIndex-1].className += "active";
+}
+
+window.addEventListener("load", function () {
+  showSlides(slideIndex);
+  myTimer = setInterval(function () { plusSlides(1) }, 4000);
+})
+
+// var autoSlideIndex = 0;
+// showAutoSlides();
+
+// function showAutoSlides () {
+//   var i;
+//   var autoSlides = document.getElementsByClassName("slides");
+//   for (i = 0; i < autoSlides.length; i++) {
+//     autoSlides[i].style.display = "none";
+//   }
+//   autoSlideIndex++;
+//   if (autoSlideIndex > autoSlides.length) {autoSlideIndex = 1}
+//   autoSlides[autoSlideIndex-1].style.display = "block";
+//   setTimeout(showAutoSlides, 2000);
+// }
+
+
+
+// var slideIndex = 1;
+
+// var myTimer;
+
+// var slideshowContainer;
+
+// window.addEventListener("load", function () {
+//   showSlides(slideIndex);
+  // myTimer = setInterval(function () { plusSlides(1) }, 4000);
+
+  //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  // slideshowContainer = document.getElementsByClassName('slider')[0];
+
+  //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
+  // slideshowContainer.addEventListener('mouseenter', pause)
+  // slideshowContainer.addEventListener('mouseleave', resume)
+// })
+
+// NEXT AND PREVIOUS CONTROL
+// function plusSlides(n) {
+//   clearInterval(myTimer);
+//   if (n < 0) {
+//     showSlides(slideIndex -= 1);
+//   } else {
+//     showSlides(slideIndex += 1);
+//   }
+
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+//   if (n === -1) {
+//     myTimer = setInterval(function () { plusSlides(n + 2) }, 4000);
+//   } else {
+//     myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+//   }
+// }
+
+//Controls the current slide and resets interval if needed
+// function currentSlide(n) {
+//   clearInterval(myTimer);
+//   myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slide");
+//   // var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) { slideIndex = 1 }
+//   if (n < 1) { slideIndex = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
   // for (i = 0; i < dots.length; i++) {
   //   dots[i].className = dots[i].className.replace(" active", "");
   // }
-  slides[slideIndex - 1].style.display = "block";
+  // slides[slideIndex - 1].style.display = "block";
   // dots[slideIndex - 1].className += " active";
-}
+// }
 
 // pause = () => {
 //   clearInterval(myTimer);
